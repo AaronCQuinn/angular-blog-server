4feec80f0d713e38716ea35671a190de78b7044b
Aaron Quinn <aaron.quinn.nfld@gmail.com>
2023-03-14 14:01:28 -0230

    change to fetching blogs

--- routes/blogs.js
+++ routes/blogs.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
     try {
       const getBlogs = await Blog.find({
         $or: [
-          { postedBy: "anonymous", createdAt: { $gt: threeDayFilter } },
+          { postedBy: "anonymous", createdAt: { $lt: threeDayFilter } },
           { postedBy: { $ne: "anonymous" } }
         ]
       }).sort({ createdAt: -1 });
